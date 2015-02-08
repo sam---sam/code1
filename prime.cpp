84e0992157916edbdefe20f2b003b63399735dc9
sam---sam <sam---sam@users.noreply.github.com>
2015-02-08 07:54:16 -0500

    Create prime.cpp

---
+++ prime.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+bool isPrime (int n){return (n%2!=0 && n%3!=0 && n%5!=0 );}
+
+int main (int argc , char * argv[]){
+
+for (int i=1;i<argc;i++)
+if (isPrime(atoi(argv[i])))
+cout <<argv[i]<<endl;
+return 0;
+}
